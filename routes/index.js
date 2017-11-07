1df746b28b0419f3541549b463ac673036aadda4
Chan Chung <chan.chung@moodys.com>
2017-11-06 21:15:13 -0500

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var standupCtrl = require('../controllers/standup.server.controller')
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  return standupCtrl.list(req, res);
+});
+/* GET New Note page. */
+router.get('/newnote', function(req, res) {
+    return standupCtrl.getNote(req, res);
+});
+
+/* POST New Note page. */
+router.post('/newnote', function(req, res) {
+    return standupCtrl.create(req, res);
+});
+
+router.post('/', function(req, res) {
+    return standupCtrl.filterByMember(req, res);
+});
+module.exports = router;
